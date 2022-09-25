9b3ce41d2aa1c3934dc64ad4669598bb8df85e9a
Hongbo Zheng <hongboz2@illinois.edu>
2022-09-25 15:52:10 -0500

    :ambulance: Fix MP1 :bug: :bug: :bug:

--- MP1/userapp.c
+++ MP1/userapp.c
@@ -1,28 +1,38 @@
 #include "userapp.h"
 
-int main(int argc, char* argv[])
-{
-	pid_t pid = getpid();
+int main(int argc, char* argv[]) {
+    uint32_t expire = EXPIRE;
+    // Reference: https://stackoverflow.com/questions/7550269/what-is-timenull-in-c
+    time_t start_time = time(NULL);
+
+    if (argc > 2) {
+        printf("[USAGE]: ./userapp <expire>\n");
+        exit(1);
+    } else if (argc == 2) {
+        if (atoi(argv[1]) < 0) {
+            printf("[ERROR]: Time expiration length cannot be negative number\n");
+        }
+        expire = atoi(argv[1]);
+    }
+
+    // Reference: https://www.gnu.org/software/libc/manual/html_node/Process-Identification.html#:~:text=The%20pid_t%20data%20type%20is,ID%20of%20the%20current%20process.
+    pid_t pid = getpid();
 	FILE *status_file = fopen("/proc/mp1/status", "w");
 	if (status_file) {
 		fprintf(status_file, "%d", pid);
 		fclose(status_file);
-	}
-	uint64_t n, i, random;
-	time_t start = time(NULL);
-	while (time(NULL) - start < 10) {
+	} else {
+        printf("[ERROR]: Cannot open file /proc/mp1/status\n");
+    }
+    
+    printf("[INFO]: Executing ./userapp %u with PID: %d\n", expire, pid);
+	while ((int)(time(NULL) - start_time) < expire) {
 		//random = rand() % 64;
 		//n = 1;
 		//for (i = 1; i <= random; i++)
 		//	n *= i;
 		//printf("%lu! = %lu\n", random, n);
 	}
-	int fd = open("/proc/mp1/status", O_RDONLY);
-	if (fd >= 0) {
-		char buffer[BUFSIZ];
-		read(fd, buffer, BUFSIZ);
-		//puts(buffer);
-		close(fd);
-	}
+    printf("[INFO]: Finish executing ./userapp %u with PID: %d\n", expire, pid);
 	return 0;
 }
--- MP1/userapp.h
+++ MP1/userapp.h
@@ -1,8 +1,15 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdint.h>
-#include <time.h>
+#ifndef USERAPP_H
+#define USERAPP_H
+
+#include <stddef.h>     // lib for NULL
+#include <stdio.h>      // lib for printf
+#include <stdlib.h>     // lib for exit
+#include <stdint.h>     // lib for uint64_t
+#include <sys/types.h>  // lib for time_t
+#include <time.h>       // lib for time
+#include <fcntl.h>      // lib for open
+#include <unistd.h>     // lib for getpid
+
+#define EXPIRE  10      // default timer expire time
+
+#endif

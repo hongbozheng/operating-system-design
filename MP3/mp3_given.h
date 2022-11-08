15e3e391ab40fc12cac2e8399fbfd93ddd75c07d
Hongbo Zheng <hongboz2@illinois.edu>
2022-11-07 22:57:41 -0600

    :art: Clean up mp3_given.h :art:

--- MP3/mp3_given.h
+++ MP3/mp3_given.h
@@ -9,7 +9,7 @@ struct task_struct* find_task_by_pid(unsigned int nr) {
     rcu_read_lock();
     task=pid_task(find_vpid(nr), PIDTYPE_PID);
     rcu_read_unlock();
-    if(task == NULL)
+    if (task == NULL)
         printk(KERN_INFO "find_task_by_pid: couldnt find pid %d\n", nr);
     return task;
 }
@@ -24,7 +24,7 @@ int get_cpu_use(int pid, unsigned long *min_flt, unsigned long *maj_flt,
     struct task_struct* task;
     rcu_read_lock();
     task=find_task_by_pid(pid);
-    if(task!=NULL) {
+    if (task!=NULL) {
         *min_flt=task->min_flt;
         *maj_flt=task->maj_flt;
         *utime=task->utime;
